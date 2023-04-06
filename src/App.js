8eba15004dd6e1fea692d97e4d863ac0f9338021
baqar-abbas <baqar007@hotmail.com>
2023-04-06 21:59:38 +0500

    Book store Initialization
    Book Store Initized with array of tree objects, Basic styling done, React components created and routing defined to create MVP

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+// import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Categories from './components/Categories';
+// import {
+//   createBrowserRouter,
+//   Route,
+//   createRoutesFromElements,
+//   RouterProvider,
+// } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar />}>
+      <Route index element={<Home />} />
+      <Route path="/Categories" element={<Categories />} />
+    </Route>,
+  ),
+);
+
+function App() {
+  return (
+    <>
+    <RouterProvider router={router} />
+    {/* <Home />
+    <Categories /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookList = ({ books }) => (
+  <div className="book-wrap">
+    {books.map((book) => (
+      <div className="books" key={book.id}>
+        <ul>
+          <li>{book.title}</li>
+          <li style={{ paddingLeft: '6rem' }}>
+            <span style={{ fontWeight: '700', fontSize: '20px' }}> by </span>
+            {book.author}
+          </li>
+        </ul>
+      </div>
+    ))}
+  </div>
+);
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default BookList;
